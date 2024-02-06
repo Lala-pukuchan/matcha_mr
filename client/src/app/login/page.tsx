fd8776998a4882451409535991ede5cfd5dba048
Lala-pukuchan <ruruover1105@gmail.com>
2024-02-06 21:07:27 +0100

    creating login form

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,7 +1,47 @@
+import { FormEvent } from "react";
+
 export default function login() {
+  //async function onSubmit(event: FormEvent<HTMLFormElement>) {
+  //  event.preventDefault();
+  //  const formData = new FormData(event.currentTarget);
+  //  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/login`, {
+  //    method: "POST",
+  //    body: formData,
+  //  });
+  //  const data = await response.json();
+  //  console.log(data);
+  //}
   return (
-    <div className="flex justify-center">
-      <div>just test</div>
+    <div>
+      {/*<form onSubmit={onSubmit} className="container mx-auto w-screen">*/}
+      <form className="container mx-auto w-screen">
+        <div className="flex flex-col m-10 space-y-4">
+          <label htmlFor="username">username</label>
+          <input
+            type="username"
+            id="username"
+            name="username"
+            placeholder="username"
+            required
+            className="bg-gray-100 p-3 rounded"
+          />
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="password"
+            required
+            className="bg-gray-100 p-3 rounded"
+          />
+          <button
+            type="submit"
+            className="w-40 h-9 rounded bg-pink-400 text-white"
+          >
+            Login
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
