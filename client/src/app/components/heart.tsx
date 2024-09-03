9981e282c6de8ce7ad6ba1f096dd558aedc8cf17
Lala-pukuchan <ruruover1105@gmail.com>
2024-09-03 18:58:19 +0900

    hurt size

--- client/src/app/components/heart.tsx
+++ client/src/app/components/heart.tsx
@@ -51,23 +51,25 @@ const Heart = ({ likeFromUserId, likeToUserId, alreadyLiked }) => {
 
   return (
     <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className="w-10 h-10"
-        fill={isClicked ? "pink" : "none"}
-        onClick={like}
-        style={{ cursor: "pointer" }}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-        />
-      </svg>
-      {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>}
+      <div className="container">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+          fill={isClicked ? "pink" : "none"}
+          onClick={like}
+          style={{ cursor: "pointer" }}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+          />
+        </svg>
+        {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>}
+      </div>
     </>
   );
 };
